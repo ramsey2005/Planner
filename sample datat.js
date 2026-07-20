da3f04c8fe0f2c988a89457e0e68451637335b23
ramsey2005 <44447113+ramsey2005@users.noreply.github.com>
2026-07-20 16:42:40 -0400

    sample data

---
+++ sample datat.js
@@ -0,0 +1,13 @@
+
+[
+  {
+    "productLine": "GE Labor",
+    "laborRate": 150,
+    "annualHours": 2080
+  },
+  {
+    "productLine": "Purchase Service",
+    "laborRate": 200,
+    "annualHours": 2080
+  }
+]
